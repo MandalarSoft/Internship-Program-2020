06cfd17f1a5d403ec93f6a97c4df0342347c0427
MandalarSoft <51107119+MandalarSoft@users.noreply.github.com>
2020-08-12 02:45:08 +0630

    Update load.js

--- script/app/load.js
+++ script/app/load.js
@@ -2,7 +2,7 @@ import {student as student} from "./template.js";
 import {dataSet as dataSet} from './data.js';
 
 let sourceUrl = "https://slack.com/api/users.list";
-let token = 'xoxp-1261160819365-1262533222802-1318702113648-0c37398ebfe046a334ab6b4563f32acd';
+let token = 'xoxp-1261160819365-1262533222802-1280267226631-ddbd0b21fa875a78e91205d22c348c98';
 let request = sourceUrl + "?token=" + token;
 
 export const getData = function (element) {
